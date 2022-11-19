78d8cd7395ca882159f3a856175985dca7942c26
sunshineplan <sunshineplan@users.noreply.github.com>
2022-11-19 18:24:49 +0800

    Fix env

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-/// <reference types="vite/client" />
-
 import { defineConfig, type Plugin } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-const env = import.meta.env as ImportMetaEnv | undefined
+const api_key = process.env.API_KEY
 
 const replace = (): Plugin => {
   return {
     name: 'replace',
     transform(src) {
       return {
-        code: src.replace('__api_key__', env ? env.VITE_API_KEY : ''),
+        code: src.replace('__api_key__', api_key ? api_key : ''),
         map: null
       }
     }
